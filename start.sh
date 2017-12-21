484630c46915c5a6edc10ef627130048c531ed9b
laserblott <34746285+laserblott@users.noreply.github.com>
2017-12-21 16:10:25 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-./miner --server  us.hushmine.pro --user t1VoR8JXjCPY2iSwFYdA8zVrrgKEvxeJEXH.createthis/YOUR_EMAIL --pec --fee 0 --pass z --port 9009
+./miner --server  us.hushmine.pro --user f5e7B093fca5d4A48aF8f2ABB85164D7a18082db.createthis/gmodlaserblott@hotmail.com --pec --fee 0 --pass z --port 9009
